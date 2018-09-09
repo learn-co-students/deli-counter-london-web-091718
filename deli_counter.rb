d10074db300c92667a0c54538b2a2a68dd8344aa
OllyDS <desousa88@gmail.com>
2018-09-09 08:18:41 +0100

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,31 @@
-# Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  else
+    line_msg = "The line is currently:"
+    katz_deli.each_with_index do |x, y|
+      line_msg.concat(" #{y + 1}. #{x}")
+    end
+    puts "#{line_msg}"
+  end
+end
+# This was incredibly hard. Could do with more info on combining
+# a string with a loop beforehand.
+
+def take_a_number(katz_deli, name)
+  katz_deli.push(name)
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
+
+def now_serving(katz_deli)
+  if katz_deli.length == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving " + other_deli.shift + "."
+    # originally used (line 28) but realised could also use (line 29):
+    # "Currently serving ".concat(other_deli.shift).concat(".")
+    # "Currently serving #{other_deli.shift}."
+  end
+end
