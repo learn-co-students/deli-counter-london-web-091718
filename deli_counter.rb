b5d7876e67d037914c89992baf32a0e4b7f7c431
Sang Song <sangyoon.song@gmail.com>
2018-09-09 10:51:49 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,31 @@
 # Write your code here.
+
+def line(line)
+  l = line.map.with_index{|name, pos| "#{pos+1}. #{name}"}
+  
+  if line.length == 0
+    puts "The line is currently empty."
+
+  else
+    puts "The line is currently: #{l.join(" ")}"
+  end
+  
+end
+
+def take_a_number(line, name)
+  if line.length == 0
+    line.push(name)
+    puts "Welcome, #{name}. You are number #{line.length} in line."
+  else
+    line.push(name)
+    puts "Welcome, #{name}. You are number #{line.length} in line."
+  end
+end
+
+def now_serving(line)
+  if line.length == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{line.shift}."
+  end
+end
